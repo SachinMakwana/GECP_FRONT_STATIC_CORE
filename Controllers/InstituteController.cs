f066905a7999edfdb7146d81a27f8b0bffcde3ca
Sachin Makwana <schnmakwana11@gmail.com>
2021-08-24 12:04:27 +0530

    Initialization
    Repository initialized

---
+++ Controllers/InstituteController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GECP_Front_End_Static.Controllers
+{
+    public class InstituteController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult CoE()
+        {
+            return View();
+        }
+        public IActionResult NewsLetter()
+        {
+            return View();
+        }
+
+        public IActionResult RTI()
+        {
+            return View();
+        }
+    }
+}
