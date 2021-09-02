4cf80fec30c61ddad7ecfd5865654b3e77eb7739
Sachin Makwana <schnmakwana11@gmail.com>
2021-09-02 16:37:39 +0530

    Pages updated
    updated

---
+++ Controllers/CampusCommitteeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GECP_Front_End_Static.Controllers
+{
+    public class CampusCommitteeController : Controller
+    {
+        public IActionResult WomenCell()
+        {
+            return View(); 
+        }
+        public IActionResult SSIP()
+        {
+            return View();
+        }
+        public IActionResult NSS()
+        {
+            return View();
+        }
+        public IActionResult AntiRagging()
+        {
+            return View();
+        }
+        public IActionResult Grievence()
+        {
+            return View();
+        }
+        public IActionResult PlacementCell()
+        {
+            return View();
+        }
+    }
+}
