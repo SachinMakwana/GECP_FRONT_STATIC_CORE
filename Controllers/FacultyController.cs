7076e6e746b983b4c15e9a2170748ef5e072aad5
Jahnvi Patel <jspatel19264@gmail.com>
2021-09-23 12:09:44 +0530

    Faculty

--- Controllers/FacultyController.cs
+++ Controllers/FacultyController.cs
@@ -1,16 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using GECP_Front_End_Static.Models;
+using System.IO;
 
 namespace GECP_Front_End_Static.Controllers
 {
     public class FacultyController : Controller
     {
+        public List<FacultyDetailsVM> FacultyData = new List<FacultyDetailsVM>();
+
+        public FacultyController()
+        {
+            var webClient = new WebClient();
+            string json = webClient.DownloadString(@"D:\GECP_FRONT_STATIC_CORE\wwwroot\js\FacultyRecords.json");
+            FacultyData = JsonConvert.DeserializeObject<List<FacultyDetailsVM>>(json);
+        }
         public IActionResult Electronics()
         {
-            return View();
+
+            var ecData = FacultyData.Where(m => m.Dept_ID == 1).ToList();
+            return View(ecData);
         }
 
         public IActionResult Computer()
@@ -46,6 +60,12 @@ namespace GECP_Front_End_Static.Controllers
         public IActionResult TEQIP()
         {
             return View();
+        }
+
+        public IActionResult FacultyInfo(int ID = 0)
+        {
+            FacultyDetailsVM data = FacultyData.Where(m => m.ID == ID).FirstOrDefault();
+            return View(data);
         }
     }
 }
--- Models/FacultyDetailsVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GECP_Front_End_Static.Controllers
-{
-    public class FacultyDetailsVM
-    {
-        public int Dept_ID { get; internal set; }
-        public int ID { get; internal set; }
-    }
-}
