ac4b22d5b738d66b8856241ef133d9663f0b7145
Sachin Makwana <schnmakwana11@gmail.com>
2023-03-23 17:14:28 +0530

    Local updates pushed globally
    TPO, Faculty json and newsletters

---
+++ Controllers/NewsLetterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GECP_Front_End_Static.Controllers
+{
+    public class NewsLetterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
