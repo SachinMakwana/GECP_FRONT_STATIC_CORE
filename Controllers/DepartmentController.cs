065b484719ac6499b609b9986b191b920bcc945b
Sachin Makwana <schnmakwana11@gmail.com>
2024-07-07 17:04:04 +0530

    Update DepartmentController.cs

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,32 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GECP_Front_End_Static.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace GECP_Front_End_Static.Controllers
 {
     public class DepartmentController : Controller
     {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public List<FacultyDetailsVM> FacultyData = new List<FacultyDetailsVM>();
+
+        public DepartmentController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string jsonpath = webRootPath + @"\js\FacultyRecords.json";
+
+            var webClient = new WebClient();
+            string json = webClient.DownloadString(jsonpath);
+            FacultyData = JsonConvert.DeserializeObject<List<FacultyDetailsVM>>(json);
+        }
         public IActionResult EC()
         {
-            return View();
+            var ecData = FacultyData.Where(m => m.Dept_ID == 1).OrderBy(m => m.ID).ToList();
+            return View(ecData);
         }
         public IActionResult Computer()
         {
-            return View();
+            var compData = FacultyData.Where(m => m.Dept_ID == 2).OrderBy(m => m.ID).ToList();
+            return View(compData);
         }
         public IActionResult Electrical()
         {
-            return View();
+            var elecData = FacultyData.Where(m => m.Dept_ID == 3).OrderBy(m => m.ID).ToList();
+            return View(elecData);
         }
         public IActionResult Civil()
         {
-            return View();
+            var civilData = FacultyData.Where(m => m.Dept_ID == 4).OrderBy(m => m.ID).ToList();
+            return View(civilData);
         }
+
         public IActionResult Mechanical()
         {
-            return View();
+            var mechData = FacultyData.Where(m => m.Dept_ID == 5).OrderBy(m => m.ID).ToList();
+            return View(mechData);
         }
         public IActionResult Library()
         {
@@ -42,12 +65,14 @@ namespace GECP_Front_End_Static.Controllers
         }
         public IActionResult General()
         {
-            return View();
+            var genData = FacultyData.Where(m => m.Dept_ID == 6).OrderBy(m => m.ID).ToList();
+            return View(genData);
         }
 
         public IActionResult Applied()
         {
-            return View();
+            var genData = FacultyData.Where(m => m.Dept_ID == 8).OrderBy(m => m.ID).ToList();
+            return View(genData);
         }
     }
 }
