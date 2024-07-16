8fdd50c731318d1d7fbf046922e473edfd7f56e2
Sachin Makwana <schnmakwana11@gmail.com>
2024-07-16 16:42:55 +0530

    Tender related changes
    Home page marquee, tender and testimonials changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,47 @@
 ﻿using GECP_Front_End_Static.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace GECP_Front_End_Static.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public List<MarqueeModelVM> MarqueeData = new List<MarqueeModelVM>();
+        public List<TestimonialModelVM> TestimonialData = new List<TestimonialModelVM>();
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IWebHostEnvironment hostingEnvironment)
         {
-            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string marqueejsonpath = webRootPath + @"\js\MarqueeContent.json";
+            string testimonialjsonpath = webRootPath + @"\js\TestimonialsData.json";
+
+            var webClient = new WebClient();
+            string json = webClient.DownloadString(marqueejsonpath);
+            MarqueeData = JsonConvert.DeserializeObject<List<MarqueeModelVM>>(json);
+
+            webClient = new WebClient();
+            json = webClient.DownloadString(testimonialjsonpath);
+            TestimonialData = JsonConvert.DeserializeObject<List<TestimonialModelVM>>(json);
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomePageModelVM homePageModelVM = new HomePageModelVM();
+            homePageModelVM.marqueeModelVM = MarqueeData;
+            homePageModelVM.testimonialModelVM = TestimonialData;
+            return View(homePageModelVM);
         }
 
         public IActionResult Privacy()
---
+++ Models/MarqueeModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GECP_Front_End_Static.Models
+{
+    public class MarqueeModelVM
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public bool IsFile { get; set; }
+        public string FileLink { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+    }
+
+    public class TestimonialModelVM
+    {
+        public int ID { get; set; }
+        public string StudentName { get; set; }
+        public string Department { get; set; }
+        public string PassoutYear { get; set; }
+        public string Testimonial { get; set; }
+    }
+
+    public class HomePageModelVM
+    {
+        public IList<MarqueeModelVM> marqueeModelVM { get; set; }
+        public IList<TestimonialModelVM> testimonialModelVM { get; set; }
+
+        public HomePageModelVM()
+        {
+            marqueeModelVM = new List<MarqueeModelVM>();
+            testimonialModelVM = new List<TestimonialModelVM>();
+        }
+    }
+}
