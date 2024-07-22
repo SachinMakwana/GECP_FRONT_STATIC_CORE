4dd73fb49a73f35f0d9407af97cbe9c71e334eea
Sachin Makwana <schnmakwana11@gmail.com>
2024-07-22 17:30:27 +0530

    TPO related chagnes
    TPO changes

--- Controllers/CampusCommitteeController.cs
+++ Controllers/CampusCommitteeController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GECP_Front_End_Static.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace GECP_Front_End_Static.Controllers
 {
     public class CampusCommitteeController : Controller
     {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public List<PlacementDataVM> PlacementDataVM = new List<PlacementDataVM>();
+
+        public CampusCommitteeController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string jsonpath = webRootPath + @"\PlacementCell\PlacementData.json";
+
+            var webClient = new WebClient();
+            string json = webClient.DownloadString(jsonpath);
+            PlacementDataVM = JsonConvert.DeserializeObject<List<PlacementDataVM>>(json);
+        }
         public IActionResult WomenCell()
         {
             return View(); 
@@ -30,7 +47,7 @@ namespace GECP_Front_End_Static.Controllers
         }
         public IActionResult PlacementCell()
         {
-            return View();
+            return View(PlacementDataVM);
         }
     }
 }
---
+++ Models/PlacementViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace GECP_Front_End_Static.Models
+{
+    public class PlacementViewModel
+    {
+    }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class PlaementDataYearWiseVM
+    {
+        public string Year { get; set; }
+        public int Passout { get; set; }
+        public int Placed { get; set; }
+        public int AverageCTC { get; set; }
+        public int HigherStudy { get; set; }
+        public int Business { get; set; }
+        public double PlacementPercentage { get; set; }
+        public bool IsDisplay { get; set; }
+    }
+
+    public class PlacementDataVM
+    {
+        public string BranchID { get; set; }
+        public string BranchName { get; set; }
+        public List<PlaementDataYearWiseVM> Data { get; set; }
+    }
+
+
+}
