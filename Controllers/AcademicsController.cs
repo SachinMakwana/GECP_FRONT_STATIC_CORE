e5735cdc2deffce254797727dc6d31fa18267927
Sachin Makwana <schnmakwana11@gmail.com>
2024-08-17 16:30:42 +0530

    Achievements
    Achievements

--- Controllers/AcademicsController.cs
+++ Controllers/AcademicsController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GECP_Front_End_Static.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace GECP_Front_End_Static.Controllers
 {
+   
     public class AcademicsController : Controller
     {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public List<AchievementsVM> AchievementsVM = new List<AchievementsVM>();
+
+        public AcademicsController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string jsonpath = webRootPath + @"\Achievements\Achievements.json";
+
+            var webClient = new WebClient();
+            string json = webClient.DownloadString(jsonpath);
+            AchievementsVM = JsonConvert.DeserializeObject<List<AchievementsVM>>(json);
+        }
         public IActionResult AcademicCalender()
         {
             return View();
@@ -20,7 +38,7 @@ namespace GECP_Front_End_Static.Controllers
 
         public IActionResult Achievements()
         {
-            return View();
+            return View(AchievementsVM);
         }
     }
 }
---
+++ Models/AchievementsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GECP_Front_End_Static.Models
+{
+    
+    public class AchievementsVM
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+    }
+}
