16521cb89487e60344777bca4acea5dc25ba537e
Sachin Makwana <schnmakwana11@gmail.com>
2024-09-26 17:06:24 +0530

    MOUs final change
    final push

--- Controllers/InstituteController.cs
+++ Controllers/InstituteController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GECP_Front_End_Static.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace GECP_Front_End_Static.Controllers
 {
     public class InstituteController : Controller
     {
+
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public List<MOUModel> MOUModelVM = new List<MOUModel>();
+       
+
+        public InstituteController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string jsonpath = webRootPath + @"\MOU\MPU.json";
+
+            var webClient = new WebClient();
+            string json = webClient.DownloadString(jsonpath);
+            MOUModelVM = JsonConvert.DeserializeObject<List<MOUModel>>(json);
+
+        }
         public IActionResult AboutUs()
         {
             return View();
@@ -34,7 +54,7 @@ namespace GECP_Front_End_Static.Controllers
 
         public IActionResult MoU()
         {
-            return View();
+            return View(MOUModelVM);
         }
 
         public IActionResult RTI()
---
+++ Models/MOUModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GECP_Front_End_Static.Models
+{
+       public class MOUModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string File { get; set; }
+    }
+}
