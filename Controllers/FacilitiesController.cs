7b0f7eef13b7fce8be0073abef7b600f86046352
purav1hub <puravmodi3105@gmail.com>
2025-01-07 16:25:11 +0530

    Facilities related changes
    done

---
+++ Controllers/FacilitiesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GECP_Front_End_Static.Controllers
+{
+    public class FacilitiesController : Controller
+    {
+        public IActionResult Library ()
+        {
+            return View();
+        }
+        public IActionResult Canteen()
+        {
+            return View();
+        }
+        public IActionResult Hostel()
+        {
+            return View();
+        }
+        public IActionResult Gymkhana()
+        {
+            return View();
+        }
+        public IActionResult StudentSection()
+        {
+            return View();
+        }
+        public IActionResult CentralFacilities()
+        {
+            return View();
+        }
+        public IActionResult FeesPayment()
+        {
+            return View();
+        }
+        public IActionResult StudentClub()
+        {
+            return View();
+        }
+        public IActionResult CenterOfExcellence()
+        {
+            return View();
+        }
+        public IActionResult Auditorium()
+        {
+            return View();
+        }
+    }
+}
