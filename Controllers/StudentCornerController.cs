d43db57c3d5b05671adddef8ff9d748c6564256b
NIKHIL-15-TUF <nikhil20058051@gmail.com>
2025-02-07 17:21:11 +0530

    Fresh work

---
+++ Controllers/StudentCornerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GECP_Front_End_Static.Controllers
+{
+	public class StudentCornerController : Controller
+	{
+		public IActionResult Rules()
+		{
+			return View();
+		}
+		public IActionResult TimeTable()
+		{
+			return View();
+		}
+		public IActionResult Enroll()
+		{
+			return View();
+		}
+		public IActionResult StudentGradeHistory()
+		{
+			return View();
+		}
+		public IActionResult Fees()
+		{
+			return View();
+		}
+    }
+}
