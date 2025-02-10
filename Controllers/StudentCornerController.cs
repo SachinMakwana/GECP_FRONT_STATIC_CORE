3080fe747b5cde4abaa074c536e4136e4b0b8b30
Sachin Makwana <schnmakwana11@gmail.com>
2025-02-10 17:20:19 +0530

    necessary changes
    changes

--- Controllers/StudentCornerController.cs
+++ Controllers/StudentCornerController.cs
@@ -1,20 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GECP_Front_End_Static.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace GECP_Front_End_Static.Controllers
 {
 	public class StudentCornerController : Controller
 	{
-		public IActionResult Rules()
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public List<MOUModel> MOUModelVM = new List<MOUModel>();
+        public StudentCornerController(IWebHostEnvironment hostingEnvironment)
+		{
+            _hostingEnvironment = hostingEnvironment;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string jsonpath = webRootPath + @"\MOU\MPU.json";
+
+            var webClient = new WebClient();
+            string json = webClient.DownloadString(jsonpath);
+            MOUModelVM = JsonConvert.DeserializeObject<List<MOUModel>>(json);
+        }
+        public IActionResult Rules()
 		{
 			return View();
 		}
 		public IActionResult TimeTable()
 		{
-			return View();
+			return View(MOUModelVM);
 		}
 		public IActionResult Enroll()
 		{
-			return View();
+			return View(MOUModelVM);
 		}
 		public IActionResult StudentGradeHistory()
 		{
