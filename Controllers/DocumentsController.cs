5e23f70b58017fd361025c6420d31a36f760d0a2
purav1hub <puravmodi3105@gmail.com>
2025-02-13 01:05:34 +0530

    View of all Documents page
    Done

---
+++ Controllers/DocumentsController.cs
@@ -0,0 +1,37 @@
+﻿using GECP_Front_End_Static.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GECP_Front_End_Static.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public List<DocumentsVm> DocumentsVm = new List<DocumentsVm>();
+
+        public DocumentsController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string jsonpath = webRootPath + @"\Data\Documents\Documents.json";
+
+            var webClient = new WebClient();
+            string json = webClient.DownloadString(jsonpath);
+            DocumentsVm = JsonConvert.DeserializeObject<List<DocumentsVm>>(json);
+        }
+
+
+        public IActionResult Index(int id = 0)
+        {
+            var data = DocumentsVm.Where(m => m.Id == id).FirstOrDefault();
+            data.YearSection = data.YearSection.OrderBy(m => m.Year).ToList();
+            return View(data);
+        }
+    }
+}
