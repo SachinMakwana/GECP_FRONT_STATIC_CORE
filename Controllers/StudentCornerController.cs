51d918ebc2e6afc90a445a8e203ff5c706c8edfc
purav1hub <puravmodi3105@gmail.com>
2025-02-21 01:05:46 +0530

    changes copied
    changes copied from club_page_design

--- Controllers/StudentCornerController.cs
+++ Controllers/StudentCornerController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting.Internal;
 using Newtonsoft.Json;
 using System.Net;
+using System.Linq;
 
 namespace GECP_Front_End_Static.Controllers
 {
@@ -12,15 +13,19 @@ namespace GECP_Front_End_Static.Controllers
 	{
         private readonly IWebHostEnvironment _hostingEnvironment;
         public List<MOUModel> MOUModelVM = new List<MOUModel>();
+        public Student ClubsVMs = new Student();
         public StudentCornerController(IWebHostEnvironment hostingEnvironment)
 		{
             _hostingEnvironment = hostingEnvironment;
             string webRootPath = _hostingEnvironment.WebRootPath;
             string jsonpath = webRootPath + @"\MOU\MPU.json";
+            string jsonpath1 = webRootPath + @"\Data\Facilities\StudentClub.json";
 
             var webClient = new WebClient();
             string json = webClient.DownloadString(jsonpath);
+            string json1 = webClient.DownloadString(jsonpath1);
             MOUModelVM = JsonConvert.DeserializeObject<List<MOUModel>>(json);
+            ClubsVMs = JsonConvert.DeserializeObject<Student>(json1);
         }
         public IActionResult Rules()
 		{
@@ -34,7 +39,16 @@ namespace GECP_Front_End_Static.Controllers
 		{
 			return View(MOUModelVM);
 		}
-		public IActionResult StudentGradeHistory()
+		public IActionResult StudentClubs ()
+		{
+			return View(ClubsVMs);
+		}
+        public IActionResult ClubDetails(int id)
+        {
+			var ClubVM = ClubsVMs.Clubs.FirstOrDefault(m => m.ID == id);
+            return View(ClubVM);
+        }
+        public IActionResult StudentGradeHistory()
 		{
 			return View();
 		}
