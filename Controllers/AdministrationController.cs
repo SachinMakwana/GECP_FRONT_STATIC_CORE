9793790c827ff53fe9c66fa30b7c352aef3cc3dc
Sachin Makwana <schnmakwana11@gmail.com>
2025-03-06 12:37:19 +0530

    Important Updates Administrative menu

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GECP_Front_End_Static.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public IActionResult Principal()
+        {
+            return View();
+        }
+    }
+}
