ba7804da1f415dde4d89d353176fb6286a75a3f0
purav1hub <puravmodi3105@gmail.com>
2025-09-02 18:20:30 +0530

    GeM Portal Done

---
+++ Models/Administration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+namespace GECP_Front_End_Static.Models
+{
+    public class Administration
+    {
+    }
+
+    public class CentralStoreVM
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string TitleImage { get; set; }
+        public string About { get; set; }
+        public string Logo { get; set; }
+        public List<string> About_Images { get; set; }
+        public List<string> Images { get; set; } = new List<string>();
+        public string BlogLink { get; set; }
+        public string Link { get; set; }
+        public bool ShowDocument { get; set; } = false;
+        public TabTitles TabTitles { get; set; } = new TabTitles();
+    }
+}
